5e336185afe87b29f7d30dc8b5f84b482095cc52
世元 李 <1498045907@qq.com>
2022-03-28 21:47:46 +0800

    Init

--- app/src/main/java/com/acite/localtransfer/ConvService.java
+++ app/src/main/java/com/acite/localtransfer/ConvService.java
@@ -1,5 +1,12 @@
 package com.acite.localtransfer;
 
-public class ConvService {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class ConvService {
+    private ServerSocket Server;
+    public ConvService() throws IOException
+    {
+        Server = new ServerSocket(5544);
+    }
 }
